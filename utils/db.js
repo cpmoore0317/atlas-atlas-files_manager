dffd264f4027b24948a38bb2afcde556562561ae
cpmoore0317 <cpmoore317@gmail.com>
2024-07-11 11:31:15 -0500

    Testing mongodb implementation

--- utils/db.js
+++ utils/db.js
@@ -5,37 +5,32 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}/${database}`;
 
-    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.db = null;
+    this.url = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Connect to MongoDB and set the `db` property
     this.client.connect()
-      .then(() => {
-        console.log('Connected to MongoDB');
-        this.db = this.client.db(database);
-      })
-      .catch((err) => {
-        console.error(`MongoDB connection error: ${err}`);
-      });
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((err) => console.error(`MongoDB connection error: ${err}`));
+
+    this.db = this.client.db(database);
   }
 
-  // Use the `topology` property to check connection state
-  isAlive() {
-    return this.client.topology?.isConnected();
+  async isAlive() {
+    try {
+      await this.client.db().command({ ping: 1 });
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 
-  // Fetch the number of documents in the 'users' collection
   async nbUsers() {
-    if (!this.db) throw new Error('Not connected to MongoDB');
     const usersCollection = this.db.collection('users');
     return usersCollection.countDocuments();
   }
 
-  // Fetch the number of documents in the 'files' collection
   async nbFiles() {
-    if (!this.db) throw new Error('Not connected to MongoDB');
     const filesCollection = this.db.collection('files');
     return filesCollection.countDocuments();
   }
