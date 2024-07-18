51222608dd9438bf8f0161a43b025f68ce8ae25b
cpmoore0317 <cpmoore317@gmail.com>
2024-07-18 10:14:58 -0500

    Up to date test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+// routes/index.js
+
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController'); // Add this line
+const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -11,6 +13,8 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload); // Add this line
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 module.exports = router;
