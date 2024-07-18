6b2b73fbcb1b4f1efdd228fdfa0f172cead91f81
cpmoore0317 <cpmoore317@gmail.com>
2024-07-18 10:20:59 -0500

    Added the logic for publishing and unpublishing files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -57,6 +57,74 @@ const FilesController = {
     ]).toArray();
 
     return res.status(200).json(files);
+  },
+
+  async putPublish(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const fileId = req.params.id;
+    const file = await mongoose.connection.db.collection('files').findOne({
+      _id: mongoose.Types.ObjectId(fileId),
+      userId: mongoose.Types.ObjectId(userId)
+    });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    await mongoose.connection.db.collection('files').updateOne(
+      { _id: mongoose.Types.ObjectId(fileId), userId: mongoose.Types.ObjectId(userId) },
+      { $set: { isPublic: true } }
+    );
+
+    const updatedFile = await mongoose.connection.db.collection('files').findOne({
+      _id: mongoose.Types.ObjectId(fileId),
+      userId: mongoose.Types.ObjectId(userId)
+    });
+
+    return res.status(200).json(updatedFile);
+  },
+
+  async putUnpublish(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const fileId = req.params.id;
+    const file = await mongoose.connection.db.collection('files').findOne({
+      _id: mongoose.Types.ObjectId(fileId),
+      userId: mongoose.Types.ObjectId(userId)
+    });
+
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    await mongoose.connection.db.collection('files').updateOne(
+      { _id: mongoose.Types.ObjectId(fileId), userId: mongoose.Types.ObjectId(userId) },
+      { $set: { isPublic: false } }
+    );
+
+    const updatedFile = await mongoose.connection.db.collection('files').findOne({
+      _id: mongoose.Types.ObjectId(fileId),
+      userId: mongoose.Types.ObjectId(userId)
+    });
+
+    return res.status(200).json(updatedFile);
   }
 };
 
